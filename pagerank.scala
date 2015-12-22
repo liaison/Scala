0e4361c7f50056903e1ce396a589afa68bcdf21e
Lisong GUO <lisong.guo@amadeus.com>
2015-12-22 17:06:37 +0100

    init PageRank in scala

---
+++ pagerank.scala
@@ -0,0 +1,102 @@
+
+import scala.io.Source
+import scala.collection.immutable.List
+import scala.collection.immutable.Map
+
+/**
+ *  Implementation of PageRank alogorithm,
+ *    with the damping factor as 0.85, 
+ *      i.e. the possibility that a surfer randomly clicks on a page.
+ *
+ *  Input file format:
+ *      URL  outbound_URL
+ *
+ *  The two URLs are separated by spaces.
+ *
+ */
+object PageRank {
+
+    /**
+     *  Parse the input, and construct the essential data structure 
+     *      which would be used for the later iteration.
+     *
+     *    Map[url, List[neighbor_url]]
+     */
+    def url_map(input : List[String]) : Map[String, List[String]] ={
+        /** Basically, doing .groupByKey() SQL operation to obtain the map: 
+                Map[url, List[neighbor_urls]] */
+        val url_neighbors = input.map{
+            line => val pair = line.split("\\s+")
+            (pair(0), pair(1))
+        }.groupBy{case (p0, p1) => p0}.map{ case (k, group) =>
+            val neighbor_list = group.map{ case (p0, p1) => p1 }.toList
+            (k, neighbor_list)
+        }
+
+        url_neighbors
+    }
+
+    /**
+     *  Initialize the initial PageRank value (i.e. 1F) for each URL.
+     */
+    def init_rank(url_map : Map[String, List[String]])
+        : Map[String, Float] = {
+
+        // Set the initial PageRank value as 1
+        url_map.map{case (url, neighbors) => (url, 1F)}
+        
+    }
+    
+    def page_ranking(url_map  : Map[String, List[String]],
+                     rank_map : Map[String, Float],
+                     iter     : Int)
+        : Map[String, Float] = {
+        
+        var new_rank_map = rank_map
+        
+        for (i <- 0 to iter ) {
+            val contribs_list = url_map.flatMap{ case (url, neighbors) =>
+                val contrib = new_rank_map(url) * 0.85F / neighbors.length
+                neighbors.map( neighbor => (neighbor, contrib) ).toList
+            }
+    
+            new_rank_map = contribs_list.groupBy{ case (t0, t1) => t0 }.
+                map{ case (k, group) => 
+                    val value_list = group.map{ case (p0, p1) => p1 }.toList
+                    (k, 0.15F + value_list.foldLeft(0F)(_+_))
+                }
+        
+        }
+
+        // Return the new page rank values.
+        new_rank_map
+    }
+
+    def process(input : List[String]) {
+        
+        val url_neighbors_map = url_map(input)
+        
+        print(url_neighbors_map)
+        
+        val page_rank_map = init_rank(url_neighbors_map)
+        
+        print(page_rank_map)
+        
+        // number of iteration
+        val iter = 10
+        print(page_ranking(url_neighbors_map, page_rank_map, iter))
+    }
+
+    def main(args : Array[String]) {
+    
+        if (args.length > 0) { 
+            val input = Source.fromFile(args(0)).getLines.toList
+            
+            process(input)
+            
+        } else {
+            Console.err.println("Error: missing the links mapping file!")
+        }
+    }
+
+}
