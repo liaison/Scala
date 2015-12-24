af41e1d40f321ea3612c8b3523fe4d583177ee92
Lisong Guo <lisong.guo@amadeus.com>
2015-12-24 11:52:26 +0100

    joggling between mutable and immutable.

--- pagerank.scala
+++ pagerank.scala
@@ -1,7 +1,9 @@
 
 import scala.io.Source
 import scala.collection.immutable.List
-import scala.collection.immutable.Map
+
+import scala.collection.mutable.Map
+import scala.collection.mutable.Set
 
 /**
  *  Implementation of PageRank alogorithm,
@@ -20,31 +22,39 @@ object PageRank {
      *  Parse the input, and construct the essential data structure 
      *      which would be used for the later iteration.
      *
+     *    Set[url]
      *    Map[url, List[neighbor_url]]
      */
-    def url_map(input : List[String]) : Map[String, List[String]] ={
+    def parse(input : List[String]) : 
+        (Set[String], Map[String, List[String]]) ={
+        
+        val url_set = Set[String]()
+        
         /** Basically, doing .groupByKey() SQL operation to obtain the map: 
                 Map[url, List[neighbor_urls]] */
-        val url_neighbors = input.map{
+        var url_neighbors = input.map{
             line => val pair = line.split("\\s+")
             (pair(0), pair(1))
         }.groupBy{case (p0, p1) => p0}.map{ case (k, group) =>
-            val neighbor_list = group.map{ case (p0, p1) => p1 }.toList
+            val neighbor_list = group.map{ case (p0, p1) => url_set ++ List(p0, p1); p1 }.toList
             (k, neighbor_list)
         }
 
-        url_neighbors
+        (url_set, Map() ++ url_neighbors)
     }
 
     /**
      *  Initialize the initial PageRank value (i.e. 1F) for each URL.
      */
-    def init_rank(url_map : Map[String, List[String]])
+    def init_rank(url_set : Set[String])
         : Map[String, Float] = {
-
-        // Set the initial PageRank value as 1
-        url_map.map{case (url, neighbors) => (url, 1F)}
+   
+        val page_rank_map = Map[String, Float]()
         
+        // Set the initial PageRank value as 1
+        url_set.foreach{ url => page_rank_map + (url -> 1F) }
+        
+        page_rank_map
     }
     
     def page_ranking(url_map  : Map[String, List[String]],
@@ -60,7 +70,10 @@ object PageRank {
                 neighbors.map( neighbor => (neighbor, contrib) ).toList
             }
     
-            new_rank_map = contribs_list.groupBy{ case (t0, t1) => t0 }.
+            print(contribs_list)
+            
+            new_rank_map = Map() ++
+                contribs_list.groupBy{ case (t0, t1) => t0 }.
                 map{ case (k, group) => 
                     val value_list = group.map{ case (p0, p1) => p1 }.toList
                     (k, 0.15F + value_list.foldLeft(0F)(_+_))
@@ -69,17 +82,18 @@ object PageRank {
         }
 
         // Return the new page rank values.
-        new_rank_map
+        Map() ++ new_rank_map
     }
 
     def process(input : List[String]) {
         
-        val url_neighbors_map = url_map(input)
+        val (url_set, url_neighbors_map) = parse(input)
         
         print(url_neighbors_map)
         
-        val page_rank_map = init_rank(url_neighbors_map)
+        val page_rank_map = init_rank(url_set)
         
+        print("init ranking:")
         print(page_rank_map)
         
         // number of iteration
