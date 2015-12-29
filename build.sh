b7b21e774be69d32b188fd19c41eda2dbd6a41b6
Lisong Guo <lisong.guo@amadeus.com>
2015-12-29 13:36:40 +0100

    script to facilitate the compiling

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# 
+# This script helps with the compiling of source files.
+# It would compile all the source files in current directory, unless otherwise specified.
+# 
+
+# compiler
+CC=scalac
+
+# the compiling output directory
+OUT=target
+
+if [ ! -d $OUT ]; then
+    mkdir $OUT
+fi
+
+if [ $# -gt 0 ]; then
+    SRC=$@
+else
+    SRC=`ls *.scala`
+fi
+
+
+cd $OUT
+
+for file in $SRC; do
+    echo "compile $file ..."
+    $CC ../$file
+done
+
+
