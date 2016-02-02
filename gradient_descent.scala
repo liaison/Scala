5a1e5feac32a3dba63fc1b1af76f48b8e2d06bca
Lisong G <lisong.guo@icloud.com>
2016-02-02 23:13:05 +0100

    with the same number of iterations (~2000) but less computation, the stochastic gradient descent can achieve the same result as the gradient descent, but higher precision (1.0E-4 vs 1.0E-6)

--- gradient_descent.scala
+++ gradient_descent.scala
@@ -19,11 +19,11 @@ object GradientDescent {
      *  where y is the dependent variable,
      *  and X is the vector of the independent variables.
      */
-    def parse(input: List[String]) : List[(Double, Array[Double])] = {
+    def parse(input: List[String]) : Array[(Double, Array[Double])] = {
         input.map{ s =>
           val yX = s.split(",").map(_.trim.toDouble)
           (yX.head, yX.tail)
-        }
+        }.toArray
     }
 
     /**
@@ -31,9 +31,10 @@ object GradientDescent {
      *  @return the weight vector of the linear regression.
      */
     def gradient_descent(theta: Array[Double],
-                         points: List[(Double, Array[Double])],
+                         points: Array[(Double, Array[Double])],
                          learningRate: Double,
-                         precision: Double)
+                         precision: Double,
+                         stochastic: Boolean = false)
       : Array[Double] = {
 
       var theta_old = theta
@@ -43,7 +44,7 @@ object GradientDescent {
       do {
         iteration += 1
 
-        val gradients = calculate_gradients(theta_old, points)
+        val gradients = calculate_gradients(theta_old, points, stochastic)
  
         val theta_new =
           theta_old.zip(gradients).map{ case (theta, gradient) =>
@@ -60,6 +61,7 @@ object GradientDescent {
 
       println(s"learningRate:${learningRate}\t" +
               s"precision:${precision}\t" +
+              s"stochastic:${stochastic}\t" +
               s"number of iterations: ${iteration}")
 
       // return the new weight vector 
@@ -70,12 +72,20 @@ object GradientDescent {
      *  calculate the gradients given the weight vector and input.
      */
     def calculate_gradients(theta: Array[Double],
-                            points: List[(Double, Array[Double])])
+                            points: Array[(Double, Array[Double])],
+                            stochastic: Boolean = false)
       : Array[Double] = {
 
       val gradients = new Array[Double](theta.size)
 
-      points.foreach{ case (y, x_vec) =>
+      val samples = if(stochastic) {
+        val randomPick = (new scala.util.Random).nextInt(points.size)
+        Array(points(randomPick))
+      } else {
+        points
+      }
+
+      samples.foreach{ case (y, x_vec) =>
         val diff = h_func(theta, x_vec) - y
         gradients(0) += diff
 
@@ -84,7 +94,7 @@ object GradientDescent {
         }
       }
 
-      val m = points.size
+      val m = samples.size
       // average out the accumulated gradient
       gradients.map{ case gradient => gradient / m }
     }
@@ -101,12 +111,19 @@ object GradientDescent {
     }
 
 
-    def process(input: List[String], learningRate: Double, precision: Double) {
+    def process(input: List[String],
+                learningRate: Double,
+                precision: Double,
+                stochastic: Boolean = false) {
       val points = parse(input)
       val theta_init = new Array[Double](points.head._2.size + 1)
       val theta_new =
-        gradient_descent(theta_init, points, learningRate, precision)
-    
+        gradient_descent(theta_init,
+                         points,
+                         learningRate,
+                         precision,
+                         stochastic)
+
       Utils.print_array("gradient_descent results:", theta_new, "\t")
     }
 
@@ -119,8 +136,9 @@ object GradientDescent {
             
             val learningRate = if(args.size >= 2) args(1).toDouble else 0.01
             val precision = if(args.size >=3) args(2).toDouble else 0.0001
+            val stochastic = if(args.size >=4) args(3).toBoolean else false
 
-            process(input, learningRate, precision)
+            process(input, learningRate, precision, stochastic)
 
         } else {
             Console.err.println("Missing arguments!")
